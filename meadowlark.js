fdcf95df453a10a9419c3e6a48ece0f6ce29a59f
Américo Augusto <americoaugusto12@hotmail.com>
2023-01-15 15:15:15 -0300

    initial commit

---
+++ meadowlark.js
@@ -0,0 +1,47 @@
+const express = require('express') // importando o express de node_modules
+const expressHandlebars = require('express-handlebars')
+const port = process.env.PORT || 3000
+const app = express()
+
+// configura o view engine handlebars
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main',
+}))
+
+app.set('view engine', 'handlebars')
+
+app.get('/', (req,res) => res.render('home')) // get para adicionar rota. A função que eu forneci será chamada quando a rota for acionada
+   
+app.get('/about', (req, res) => {
+    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.lenght)]
+    res.render('about', { fortune: randomFortune })
+})
+// get para adicionar rota. A função que eu forneci será chamada quando a rota for acionada
+
+app.use(express.static(__dirname + '/public'))
+
+//página 404 personalizada
+app.use((req, res) => {
+    res.type('text/plain')
+    res.status(404)
+    res.send('404 - not found')
+})
+// página 500 personalizada
+app.use((err, req, res, next) => { //middleware
+    console.log(err.message)
+    res.type('text/plain')
+    res.status(500)
+    res.send('500 - Server Error')
+})
+
+const fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you dont't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simples."
+]
+app.listen(port, () => console.log(
+    `Express started on http://localhost:${port}; 
+    press Ctrl + C to terminate.`
+))
