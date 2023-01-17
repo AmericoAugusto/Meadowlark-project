5c2e54577dac75ec8c233b2fbe80201dae908540
Américo Augusto <americoaugusto12@hotmail.com>
2023-01-17 13:00:37 -0300

    delete upload content

--- meadowlark.js
+++ meadowlark.js
@@ -5,20 +5,7 @@ const app = express();
 const handlers = require("./lib/handlers");
 const fortune = require("./lib/middleware/fortune");
 const weatherMiddlware = require("./lib/middleware/weather");
-const multiparty = require('multiparty')
 
-app.post('/contest/vacation-photo/:year/:month', (req,res) => {
-  const form = new multiparty.Form()
-  form.parse(req, (err,fields, files) => {
-    if(err) return res.status(500).send({error: err.message})
-    handlers.vacationPhotoContestProcess(req, res, fields, files)
-  })
-})
-exports.vacationPhotoContestProcess = (req,res,fields,files) => {
-  console.log('field data: ', fields)
-  console.log('files: ', files)
-  res.redirect(303, 'contest/vacation-photo-thank-you')
-}
 // configure Handlebars view engine
 app.engine(
   "handlebars",
