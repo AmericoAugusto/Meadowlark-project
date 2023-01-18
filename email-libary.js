173a160ac894e0830ea1da9e05d769e312768ed1
Américo Augusto <americoaugusto12@hotmail.com>
2023-01-18 20:18:00 -0300

    testing this ft

---
+++ email-libary.js
@@ -0,0 +1,6 @@
+const { credentials } = require("./config");
+const email = require("./lib/email");
+const emailService = require('./lib/email')(credentials)
+
+emailService.send(email, "Hood River tours on sale today!",
+"Get 'em while they're hot!")
