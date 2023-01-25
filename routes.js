d8751eec89a0b270eaa809e95176ecedd3c85f88
Américo Augusto <americoaugusto12@hotmail.com>
2023-01-25 19:15:25 -0300

    i've moved all routes from 'meadowlark.js' to 'routes.js'

---
+++ routes.js
@@ -0,0 +1,10 @@
+
+module.exports = app => {
+    app.get("/", (req, res) => res.render("home")); // get para adicionar rota. A função que eu forneci será chamada quando a rota for acionada
+    app.get('/set-currency/:currency', handlers.setCurrency);
+    // fetch/JSON formulário
+    app.get("/newsletter", handlers.newsletter);
+    app.get("/about", (req, res) => {
+        res.render("about", { fortune: fortune.getFortune() });
+      });
+}
