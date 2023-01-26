466f8e75324eb7263f180485d9d7dd0933e54db7
Américo Augusto <americoaugusto12@hotmail.com>
2023-01-26 01:52:11 -0300

    condicional rendering with routes

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import logo from '../src/img/logo.png';
+import './App.css';
+import React from 'react';
+import { BrowserRouter,
+          Switch,
+        Route,
+      Link } from 'react-router-dom'
+import { Router } from 'express';
+
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to Meadowlark</h2>
+      <p>Check out our "<Link to="/about">About</Link>" page!</p>
+    </div>
+  )
+}
+function About() {
+  return (<i>coming soon</i>)
+}
+function NotFound() {
+  return (<i>Not Found</i>)
+}
+
+function App() {
+  return (
+    <Router>
+    <div className='container'>
+      <header>
+        <h1>Meadowlark Travel</h1>
+        <Link to="/"><img src={logo} alt='Meadowlark Travel Logo' /></Link> 
+      </header>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route  component={NotFound} />
+      </Switch>
+      <Home />
+    </div>
+    </Router>
+  )
+}
+export default App;
