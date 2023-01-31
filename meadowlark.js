57e253a0e6c31c6e275bb6185679121e8f4eac5b
Américo Augusto <americoaugusto12@hotmail.com>
2023-01-30 22:27:44 -0300

    removing some middlwares

--- meadowlark.js
+++ meadowlark.js
@@ -1,79 +1,37 @@
-const express = require("express"); // importando o express de node_modules
-const expressHandlebars = require("express-handlebars");
-const bodyParser = require("body-parser");
-const app = express();
-const handlers = require("./lib/handlers");
-
-const { weatherMiddleware } = require ("./lib/middleware/weather");
-const { credentials } = require('./config');
-
-
-const flashMiddleware = require('./lib/middleware/flash')
-const cartValidation = require('./lib/cartValidation');
-
-require('./routes')(app)
+const express = require('express')
+const bodyParser = require('body-parser')
+const multiparty = require('multiparty')
 const cors = require('cors')
 
+const handlers = require('./lib/handlers')
+
+const credentials = require('./credentials')
+
+require('./db')
+
+const app = express()
+
+app.use('/api', cors())
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 3033
+
+// eslint-disable-next-line no-undef
+app.use(express.static(__dirname + '/public'))
 
 app.get('/api/vacations', handlers.getVacationsApi)
-app.get('api/vacation/:sku', handlers.getVacationsBySkuApi)
-app.post('/api/vacations/:sku/notify-qhen-in-season',
-handlers.addVacationsInSeasonListenerApi)
+app.get('/api/vacation/:sku', handlers.getVacationBySkuApi)
+app.post('/api/vacation/:sku/notify-when-in-season', handlers.addVacationInSeasonListenerApi)
 app.delete('/api/vacation/:sku', handlers.requestDeleteVacationApi)
-app.use('/api',cors())
 
-app.use(cartValidation.resetValidation)
-app.use(cartValidation.checkWaivers)
-app.use(cartValidation.checkGuestCounts)
-
-app.use(flashMiddleware)
-
-
-
-// configure Handlebars view engine
-app.engine("handlebars", expressHandlebars.engine({
-  defaultLayout: "main",
-  helpers: {
-    section: function(name, options) {/*  only change here  */
-      if (!this._sections) {
-        this._sections = {};
-      }
-      this._sections[name] = options.fn(this);
-
-      return null;
-    }
-  }
-}));
-
-app.set("view engine", "handlebars");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-// eslint-disable-next-line no-undef
-const port = process.env.PORT || 3000;
-// eslint-disable-next-line no-undef
-app.use(express.static(__dirname + "/public"));
-app.use(weatherMiddleware);
-
-
-app.post("/api/newsletter-singup", handlers.api.newsletterSingup);
-
-
-
-//página 404 personalizada
-app.use((req, res) => {
-  res.type("text/plain");
-  res.status(404);
-  res.send("404 - not found");
-});
-// página 500 personalizada
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  //middleware
-  console.log(err.message);
-  res.type("text/plain");
-  res.status(500);
-  res.send("500 - Server Error");
-});
-
-app.listen(port, () => console.log(`Express started in ` + `${app.get('env')}
- mode at http://localhost:${port} ` + `; press CTRL-C to terminate.`))
+if(require.main === module) {
+  app.listen(port, () => {
+    console.log( `Express started on http://localhost:${port}` +
+      '; press Ctrl-C to terminate.' )
+  })
+} else {
+  module.exports = app
+}
